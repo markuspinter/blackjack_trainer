5e5c14d4b5d2d70a463c4259e5d9f333b35adcc0
markuspinter <markus.m.pinter@gmail.com>
2021-11-26 11:55:25 +0100

    index to root

--- client/js/sidebar.js
+++
@@ -1,7 +0,0 @@
-function openNav() {
-    document.getElementById("chat").style.right = "0";
-  }
-  
-  function closeNav() {
-    document.getElementById("chat").style.right = "-" + getComputedStyle(document.documentElement).getPropertyValue('--chatWidth').trim();
-  }
